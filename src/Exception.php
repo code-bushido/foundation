b6eab07f717268181fad4d01ede41ac292743c71
Wojciech Nowicki <wojciech.nowicki@iamip.com>
2019-11-17 23:28:28 +0100

    Create Exception.php

---
+++ src/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the Bushido\Foundation package.
+ *
+ * (c) Wojciech Nowicki <wnowicki@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Bushido\Foundation;
+
+class Exception extends \Exception
+{
+}
