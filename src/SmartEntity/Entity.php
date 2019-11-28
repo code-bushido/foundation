ca30a1579e993991b43ed693d0ed7b4e3438f05e
Wojciech Nowicki <wojciech.nowicki@iamip.com>
2019-11-28 22:57:57 +0100

    Create Entity.php

---
+++ src/SmartEntity/Entity.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the Bushido\Foundation package.
+ *
+ * (c) Wojciech Nowicki <wnowicki@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Bushido\Foundation\SmartEntity;
+
+use Bushido\Foundation\Contracts\EntityInterface;
+use Bushido\Foundation\Contracts\Jsonable;
+use Bushido\Foundation\Contracts\Makeable;
+
+interface Entity extends EntityInterface, Makeable, Jsonable
+{
+    public const TYPE_ARRAY = 1;
+    public const TYPE_BOOL = 2;
+    public const TYPE_INT = 4;
+    public const TYPE_FLOAT = 8;
+    public const TYPE_NUMERIC = 12;
+    public const TYPE_STRING = 16;
+    public const TYPE_OBJECT = 32;
+}
